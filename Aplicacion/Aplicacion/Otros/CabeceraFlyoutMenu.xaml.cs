298e144feb40606586309ed7e1ac6a9a885896ce
marin-8 <marin.tecnico.8@gmail.com>
2021-05-16 04:05:47 +0200

    Terminada la cabecera del flyoumenu

---
+++ Aplicacion/Aplicacion/Otros/CabeceraFlyoutMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PFG.Aplicacion
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CabeceraFlyoutMenu : StackLayout
+	{
+		public CabeceraFlyoutMenu()
+		{	
+			InitializeComponent();
+		}
+	}
+}
